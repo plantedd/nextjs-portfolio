b997cd918741378d4e2f13dd473174d76a949b49
Christian <christianparker828@gmail.com>
2023-12-06 21:28:44 -0500

    adjusted code and now works

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,32 +6,6 @@ const Footer = () => {
           <p>
             © {new Date().getFullYear()} JB&apos;s Portfolio
           </p>
-          <div className="social_icons">
-            <a
-              href="https://twitter.com/bakerj417"
-              aria-label="Twitter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-twitter"></i>
-            </a>
-            <a
-              href="https://github.com/bakerj417"
-              aria-label="GitHub"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-github"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/josephkbaker/"
-              aria-label="LinkedIn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-linkedin"></i>
-            </a>
-          </div>
         </div>
       </>
     )
